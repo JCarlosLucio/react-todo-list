56062d0a3f438e882db783c5ee0c136b8ae9003c
jcLucio <c.lucio3@gmail.com>
2020-07-07 12:23:39 -0500

    Add state for todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,6 +3,13 @@ import NewTodoForm from './NewTodoForm';
 import Todo from './Todo';
 
 export default class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+    };
+  }
+
   render() {
     return (
       <div>
