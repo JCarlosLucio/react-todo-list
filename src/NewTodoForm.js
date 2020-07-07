20c84ce827a88a84485203f2ee79859c7327c733
jcLucio <c.lucio3@gmail.com>
2020-07-07 11:53:46 -0500

    Add state for newTodo

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 export default class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: '',
+    };
+  }
   render() {
     return (
       <form>
