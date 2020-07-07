8b45f1a8b14c50083cd8e680eb4e9fbe817695b4
jcLucio <c.lucio3@gmail.com>
2020-07-07 11:51:42 -0500

    Add form for render

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 export default class NewTodoForm extends Component {
   render() {
     return (
-      <div>
-        <h1>Todo Form</h1>
-      </div>
+      <form>
+        <label htmlFor="newTodo">New Todo</label>
+        <input type="text" id="newTodo" name="newTodo" placeholder="New Todo" />
+        <button>ADD TODO</button>
+      </form>
     );
   }
 }
