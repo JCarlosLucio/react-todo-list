84c00db65e9387cabfc090ff8cb589172f8f37e1
jcLucio <c.lucio3@gmail.com>
2020-07-07 13:30:24 -0500

    Update render to use todo prop

--- src/Todo.js
+++ src/Todo.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 
 export default class Todo extends Component {
   render() {
-    return (
-      <div>
-        <h1>Todo</h1>
-      </div>
-    );
+    return <div>{this.props.todo}</div>;
   }
 }
