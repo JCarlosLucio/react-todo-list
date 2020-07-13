ed1d22787487067fc496107f52813b840c2b27ff
jcLucio <c.lucio3@gmail.com>
2020-07-13 17:36:37 -0500

    Updates

---
+++ precache-manifest.e5d668c444742fdfceb20ea9452eb627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0323d1e7d2c3b8058cc6a600cc63b1ba",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "07313c3190e8a2ba8fda",
+    "url": "/react-todo-list/static/css/main.c04c0a53.chunk.css"
+  },
+  {
+    "revision": "78b274613789f539dbb0",
+    "url": "/react-todo-list/static/js/2.bc010a9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.bc010a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07313c3190e8a2ba8fda",
+    "url": "/react-todo-list/static/js/main.575725c7.chunk.js"
+  },
+  {
+    "revision": "2c6e5d8f9eaaedf40455",
+    "url": "/react-todo-list/static/js/runtime-main.f23babde.js"
+  }
+]);
