43a975ccc4b2a22808a04aa0e029b2eea839ddba
jcLucio <c.lucio3@gmail.com>
2020-07-12 20:33:39 -0500

    Add 'Made by Lucio' link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <TodoList />
+      <div>
+        <a href="https://github.com/JCarlosLucio/todo-list">
+          Made by Lucio <i className="fab fa-github-alt" aria-hidden="true" />
+        </a>
+      </div>
     </div>
   );
 }
