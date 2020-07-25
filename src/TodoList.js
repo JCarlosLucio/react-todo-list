924ad9d9c40848b44dfbff7f503530255d82e655
jcLucio <c.lucio3@gmail.com>
2020-07-25 16:35:43 -0500

    Start TodoList refactor to use hook
    - Use useState for todos state
    - Comment previous code for reference

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import NewTodoForm from './NewTodoForm';
 import Todo from './Todo';
 import './TodoList.css';
 
-export default class TodoList extends Component {
+function TodoList() {
+  const initialTodos = [];
+  const [todos, setState] = useState(initialTodos);
+
+  return (
+    <div className="TodoList">
+      <div className="TodoList-header">
+        <h1>React Todo List</h1>
+      </div>
+      {/* <NewTodoForm addTodo={this.addTodo} /> */}
+      <ul>
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            todo={todo.todo}
+            done={todo.done}
+            // deleteTodo={this.deleteTodo}
+            // updateTodo={this.updateTodo}
+            // toggleDone={this.toggleDone}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
+
+{
+  // import React, { Component } from 'react';
+  /* export default class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -70,4 +101,5 @@ export default class TodoList extends Component {
       </div>
     );
   }
+} */
 }
