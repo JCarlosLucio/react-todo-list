72910f885bdefe481718dd98fd7cc6817246363f
jcLucio <c.lucio3@gmail.com>
2020-07-26 09:19:50 -0500

    Add state for todo w/useState

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,41 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './NewTodoForm.css';
 
-export default class NewTodoForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todo: '',
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleSubmit(evt) {
-    evt.preventDefault();
-    this.props.addTodo({ ...this.state, id: uuidv4(), done: false });
-    this.setState({ todo: '' });
-  }
-  handleChange(evt) {
-    this.setState({
-      [evt.target.name]: evt.target.value,
-    });
-  }
-  render() {
-    return (
-      <form className="NewTodoForm" onSubmit={this.handleSubmit}>
-        <label htmlFor="newTodo">New Todo</label>
-        <input
-          type="text"
-          id="newTodo"
-          placeholder="New Todo"
-          name="todo"
-          required={true}
-          value={this.state.todo}
-          onChange={this.handleChange}
-        />
-        <button>ADD TODO</button>
-      </form>
-    );
-  }
+function NewTodoForm({ addTodo }) {
+  const [todo, setTodo] = useState('');
+  return (
+    <form className="NewTodoForm">
+      {/* onSubmit={this.handleSubmit} */}
+      <label htmlFor="newTodo">New Todo</label>
+      <input
+        type="text"
+        id="newTodo"
+        placeholder="New Todo"
+        name="todo"
+        required={true}
+        // value={this.state.todo}
+        // onChange={this.handleChange}
+      />
+      <button>ADD TODO</button>
+    </form>
+  );
 }
+
+export default NewTodoForm;
+
+// import React, { Component } from 'react';
+// export default class NewTodoForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       todo: '',
+//     };
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   handleSubmit(evt) {
+//     evt.preventDefault();
+//     this.props.addTodo({ ...this.state, id: uuidv4(), done: false });
+//     this.setState({ todo: '' });
+//   }
+//   handleChange(evt) {
+//     this.setState({
+//       [evt.target.name]: evt.target.value,
+//     });
+//   }
+//   render() {
+//     return (
+//       <form className="NewTodoForm" onSubmit={this.handleSubmit}>
+//         <label htmlFor="newTodo">New Todo</label>
+//         <input
+//           type="text"
+//           id="newTodo"
+//           placeholder="New Todo"
+//           name="todo"
+//           required={true}
+//           value={this.state.todo}
+//           onChange={this.handleChange}
+//         />
+//         <button>ADD TODO</button>
+//       </form>
+//     );
+//   }
+// }
