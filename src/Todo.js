d5b529e77c96efa62348d910333fff8dd93dc7f1
jcLucio <c.lucio3@gmail.com>
2020-07-29 14:00:37 -0500

    Refactor return

--- src/Todo.js
+++ src/Todo.js
@@ -4,10 +4,9 @@ import './Todo.css';
 
 function Todo({ id, task, done, deleteTodo, updateTodo, toggleDone }) {
   const [isEditing, setIsEditing] = useState(false);
-  let result;
-  if (isEditing) {
-    result = (
-      <div className="Todo">
+  return (
+    <div className="Todo">
+      {isEditing ? (
         <EditTodoForm
           id={id}
           task={task}
@@ -15,29 +14,26 @@ function Todo({ id, task, done, deleteTodo, updateTodo, toggleDone }) {
           isEditing={isEditing}
           setIsEditing={setIsEditing}
         />
-      </div>
-    );
-  } else {
-    result = (
-      <div className="Todo">
-        <li
-          onClick={() => toggleDone(id)}
-          className={done ? 'Todo-todo done' : 'Todo-todo'}
-        >
-          {task}
-        </li>
-        <div className="Todo-btns">
-          <button onClick={() => setIsEditing(!isEditing)}>
-            <i className="fas fa-pen-square" />
-          </button>
-          <button onClick={() => deleteTodo(id)}>
-            <i className="fas fa-times-circle" />
-          </button>
-        </div>
-      </div>
-    );
-  }
-  return result;
+      ) : (
+        <React.Fragment>
+          <li
+            onClick={() => toggleDone(id)}
+            className={done ? 'Todo-todo done' : 'Todo-todo'}
+          >
+            {task}
+          </li>
+          <div className="Todo-btns">
+            <button onClick={() => setIsEditing(!isEditing)}>
+              <i className="fas fa-pen-square" />
+            </button>
+            <button onClick={() => deleteTodo(id)}>
+              <i className="fas fa-times-circle" />
+            </button>
+          </div>
+        </React.Fragment>
+      )}
+    </div>
+  );
 }
 
 export default Todo;
