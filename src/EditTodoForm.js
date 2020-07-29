b8d7b9c5e30194a4b12193c10e104918991ede6d
jcLucio <c.lucio3@gmail.com>
2020-07-29 13:48:49 -0500

    Refactor form to edit Todos to EditTodoForm

---
+++ src/EditTodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './EditTodoForm.css';
+
+function EditTodoForm({ id, task, updateTodo, isEditing, setIsEditing }) {
+  const [updatedTask, setUpdatedTask] = useState(task);
+  return (
+    <form
+      className="EditTodoForm"
+      onSubmit={(e) => {
+        e.preventDefault();
+        updateTodo(id, updatedTask);
+        setIsEditing(!isEditing);
+      }}
+    >
+      <input
+        type="text"
+        name="updatedTask"
+        placeholder="Edit Todo"
+        value={updatedTask}
+        onChange={(e) => setUpdatedTask(e.target.value)}
+      />
+      <button>Save</button>
+    </form>
+  );
+}
+
+export default EditTodoForm;
