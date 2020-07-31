39022bd4f57a2eda785f4709ad9a75cde78e1d30
jcLucio <c.lucio3@gmail.com>
2020-07-31 17:39:01 -0500

    Add useInputState custom hook
    - Add handleChange for inputs
    - Add reset to clear input

---
+++ src/hooks/useInputState.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function useInputState(initialVal) {
+  const [state, setState] = useState(initialVal);
+
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
+  const reset = () => {
+    setState('');
+  };
+
+  return [state, handleChange, reset];
+}
+
+export default useInputState;
