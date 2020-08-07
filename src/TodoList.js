727e2047058fffc70345d5830de5bafdb924d5f1
jcLucio <c.lucio3@gmail.com>
2020-08-07 11:46:04 -0500

    Add todos from TodosContext
    - Remove todo functions as they have been refactored to todo.reducer
    - Remove todos state as it has been refactored to todo.context
    - Remove useEffect as they have been refactored to useLocalStorageReducer
    - Remove initialTodos as they have been refactored to todo.context

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,52 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import NewTodoForm from './NewTodoForm';
 import Todo from './Todo';
+import { TodosContext } from './contexts/todo.context';
 import './TodoList.css';
 
 function TodoList() {
-  const initialTodos = JSON.parse(localStorage.getItem('todos')) || [];
-  const [todos, setTodos] = useState(initialTodos);
-
-  useEffect(
-    () => {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    },
-    [todos]
-  );
-
-  const addTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
-  };
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-  const updateTodo = (id, updatedTask) => {
-    const updatedTodos = todos.map(
-      (todo) => (todo.id === id ? { ...todo, task: updatedTask } : todo)
-    );
-    setTodos(updatedTodos);
-  };
-  const toggleDone = (id) => {
-    const updatedTodos = todos.map(
-      (todo) => (todo.id === id ? { ...todo, done: !todo.done } : todo)
-    );
-    setTodos(updatedTodos);
-  };
-
+  const todos = useContext(TodosContext);
   return (
     <div className="TodoList">
       <div className="TodoList-header">
         <h1>React Todo List</h1>
       </div>
-      <NewTodoForm addTodo={addTodo} />
+      <NewTodoForm />
       <ul>
         {todos.map((todo) => (
           <Todo
             key={todo.id}
             {...todo} // id, task, done
-            deleteTodo={deleteTodo}
-            updateTodo={updateTodo}
-            toggleDone={toggleDone}
           />
         ))}
       </ul>
