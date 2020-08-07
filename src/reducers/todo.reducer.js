1f7d7cbfe7c41808eeac196bd0d114cbfae081e8
jcLucio <c.lucio3@gmail.com>
2020-08-07 11:35:01 -0500

    Refactor ADD

--- src/reducers/todo.reducer.js
+++ src/reducers/todo.reducer.js
@@ -1,7 +1,9 @@
+import { v4 as uuidv4 } from 'uuid';
+
 function reducer(state, action) {
   switch (action.type) {
     case 'ADD':
-      return [...state, state.newTodo];
+      return [...state, { id: uuidv4(), task: action.task, done: false }];
     case 'DELETE':
       return state.filter((todo) => todo.id !== action.id);
     case 'UPDATE':
