1bf69edc9f5e6638024259b1b6a5174fd6e83197
jcLucio <c.lucio3@gmail.com>
2020-08-07 11:35:41 -0500

    Add TodosProvider from todo.context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import TodoList from './TodoList';
 import MadeBy from './MadeBy';
+import { TodosProvider } from './contexts/todo.context';
 
 function App() {
   return (
     <React.Fragment>
-      <TodoList />
+      <TodosProvider>
+        <TodoList />
+      </TodosProvider>
       <MadeBy link="https://github.com/JCarlosLucio/react-todo-list" />
     </React.Fragment>
   );
