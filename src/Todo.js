d824d12ca900dd2e861b15edf2d09054a2cd8599
jcLucio <c.lucio3@gmail.com>
2020-08-11 13:38:15 -0500

    Import memo from react

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, memo } from 'react';
 import useToggleState from './hooks/useToggleState';
 import EditTodoForm from './EditTodoForm';
 import { DispatchContext } from './contexts/todo.context';
